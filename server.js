00fee42a7f67ac522bcb897f071af8d4d83e377e
Millena Teles <millena.pita.teles@gmail.com>
2025-05-19 11:44:42 -0300

    final version 1.1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import fs from "noed:fs";
+import { promises as fs } from "fs"; 
 
 const PORT = 3333
 const url_database = ".database/biblioteca.json"
@@ -14,4 +14,57 @@ app.use(cors({
 
 app.use(express.json())
 
-app.post
+//lista todos
+
+app.get("/biblioteca/livros", (req, res) => {});
+
+app.get("/biblioteca/usuarios", (req, res) => {});
+
+app.get("/biblioteca/emprestimos", (req, res) => {});
+
+app.get("/biblioteca/reservas", (req, res) => {});
+
+app.get("/biblioteca/multas", (req, res) => {});
+
+//cria todos
+
+app.post("/biblioteca/usuarios/cadastro", (req, res) => {});
+
+app.post("/biblioteca/livros/cadastro", (req, res) => {});
+
+app.post("/biblioteca/emprestimos", (req, res) => {});
+
+app.post("/biblioteca/reservas", (req, res) => {});
+
+app.post("/biblioteca/multas/registrar/:emprestimoId", (req, res) => {});
+
+//lista um em especifico
+
+app.get("/biblioteca/usuarios/:id", (req, res) => {});
+
+app.get("/biblioteca/livros/:id", (req, res) => {});
+
+app.get("/biblioteca/emprestimos/:id", (req, res) => {});
+
+app.get("/biblioteca/reservas/:id", (req, res) => {});
+
+app.get("/biblioteca/multas/:emprestimoId", (req, res) => {});
+
+//atualiza as informações de determinado item
+
+app.put("/biblioteca/usuarios/:id", (req, res) => {});
+
+app.put("/biblioteca/livros/:id", (req, res) => {});
+
+app.put("/biblioteca/emprestimos/:id/devolucao", (req, res) => {});
+
+app.put("/biblioteca/multas/:id/pagar", (req, res) => {});
+
+app.put("/biblioteca/reservas/:id", (req, res) => {});
+
+//deletar um item
+
+
+app.listen(PORT, () => {
+    console.log("Servidor iniciado na porta: " + PORT)
+})
