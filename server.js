c2ad33d49c64de8fa54560258be3c617681e6219
Millena Teles <millena.pita.teles@gmail.com>
2025-06-04 11:38:00 -0300

    version 1.6

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 
 app.use(express.json())
 
-//concluido 3/31
+//concluido 14/31
 
 //lista todos
 
@@ -323,9 +323,105 @@ app.get("/biblioteca/multas/:emprestimoId", (req, res) => {});
 
 //atualiza as informações de determinado item
 
-app.put("/biblioteca/usuarios/:id", async (req, res) => {});
+app.put("/biblioteca/usuarios/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, email, endereco, senha } = req.body;
 
-app.put("/biblioteca/livros/:id", (req, res) => {});
+  try {
+    if (!nome || typeof nome !== 'string' || nome.trim() === "") {
+      return res.status(400).json({ mensagem: "O campo 'nome' é obrigatório e deve ser um texto" });
+    }
+
+    if (!email || typeof email !== 'string' || email.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'email' é obrigatório e deve ser um texto" });
+    }
+
+    if (!endereco || typeof endereco !== 'string' || endereco.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'endereço' é obrigatório e deve ser um texto" });
+    }
+
+    if (!senha || typeof senha !== 'string' || senha.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'senha' é obrigatório e deve ser um texto" });
+    }
+
+    const data = await fs.readFile(url_database, 'utf-8');
+    const database = JSON.parse(data);
+
+    const indexUsers = database.users.findIndex((user) => user.id === id);
+    if (indexUsers === -1) {
+      return res.status(404).json({ mensagem: "Usuário não encontrado" });
+    }
+
+    database.users[indexUsers] = { ...database.users[indexUsers], nome, email, endereco, senha };
+
+    await fs.writeFile(url_database, JSON.stringify(database, null, 2));
+
+    return res.status(200).json({
+      mensagem: "Usuário atualizado",
+      database: database.users[indexUsers]
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+});
+
+app.put("/biblioteca/livros/:id", async (req, res) => {
+  const {id} = req.params;
+  const {marca, editora, formato, edicao, idioma,numero_paginas, quantidade_livros_biblioteca} = req.body;
+
+  try {
+   if (!marca || typeof marca !== 'string' || marca.trim() === "") {
+      return res.status(400).json({ mensagem: "O campo 'marca' é obrigatório e deve ser um texto" });
+    }
+
+    if (!editora || typeof editora !== 'string' || editora.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'editora' é obrigatório e deve ser um texto" });
+    }
+
+    if (!formato || typeof formato !== 'string' || formato.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'formato' é obrigatório e deve ser um texto" });
+    }
+
+    if (!edicao || typeof edicao !== 'string' || edicao.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'edição' é obrigatório e deve ser um texto" });
+    }
+
+    if (!idioma || typeof idioma !== 'string' || idioma.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'idioma' é obrigatório e deve ser um texto" });
+    }
+
+    if (!numero_paginas || typeof numero_paginas !== 'string' || numero_paginas.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'numero de paginas' é obrigatório e deve ser um texto" });
+    }
+
+    if (!quantidade_livros_biblioteca || typeof quantidade_livros_biblioteca !== 'string' || quantidade_livros_biblioteca.trim() === '') {
+      return res.status(400).json({ mensagem: "O campo 'edição' é obrigatório e deve ser um quantidade de livros na biblioteca" });
+    }
+
+    const data = await fs.readFile(url_database, 'utf-8');
+    const database = JSON.parse(data);
+
+    const indexBook = database.livros.findIndex((book) => book.id === id );
+    if(indexBook === -1){
+      return res.status(404).json({mensagem: "Livro não encontrado"})
+    }
+
+    database.livros[indexBook] = {...database.livros[indexBook], marca, editora, formato, edicao, idioma,numero_paginas, quantidade_livros_biblioteca}
+    
+    await fs.writeFile(url_database, JSON.stringify(database, null, 2));
+
+    return res.status(200).json({
+      mensagem: "Livro atualizado",
+      database: database.livros[indexBook]
+    });
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({mensagem: "Internal server error"})
+  }
+});
 
 app.put("/biblioteca/emprestimos/:id/devolucao", (req, res) => {});
 
